9c5eeb80c8be3d4b57f7a0ccabad0989290c19cf
PashaSafatiuk <tarakan0@icloud.com>
2025-09-07 10:19:21 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,32 @@
+age = 5
+tasks_completed = 10
+coordinates = (25, 50)
+status = 'online'
+quest_name = 'quest'
+range_values = (15, 30)
+score = 4
+time_interval = (1, 16)
+goal = 'Learn'
+level = 10
+
+print(age)
+print(tasks_completed)
+print(coordinates)
+print(status)
+print(quest_name)
+print(range_values)
+print(score)
+print(time_interval)
+print(goal)
+print(level)
+
+print(type(age))
+print(type(tasks_completed))
+print(type(coordinates))
+print(type(status))
+print(type(quest_name))
+print(type(range_values))
+print(type(score))
+print(type(time_interval))
+print(type(goal))
+print(type(level))
